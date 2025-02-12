715e61bd9f31eccc961c692cd8eec66d2b3f17d1
janarthanan <janarthanansmj@gmail.com>
2025-02-12 14:20:45 +0530

    no change

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,16 @@
+"use client"
 import React from 'react'
 import Navbar from '@/app/components/navbar'
 
 const Home = () => {
+  const al =()=>{
+    alert('clicked')
+  }
   return (
     <div className='bg-blue-300 w-full h-screen'>
       <Navbar/>
+
+      <button onClick={al}>click</button>
     </div>
   )
 }
